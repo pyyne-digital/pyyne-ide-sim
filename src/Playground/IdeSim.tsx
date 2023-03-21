ccc297045ea4e44245758341feff3a7d08a9ffa8
morezco <guilherme@inveterate.com>
2023-03-21 16:06:16 -0300

    moves test text into data file

--- src/Playground/IdeSim.tsx
+++ src/Playground/IdeSim.tsx
@@ -1,104 +1,14 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Playground } from "../interfaces/Playground";
 
 import * as IdeSim from "../";
 import { TerminalLine } from "../Previews/Terminal/props";
 
+import * as data from "./data";
+
 interface Props {
   Playground: Playground;
 }
-
-const test = `import * as Pyyne from 'interfaces/PYYNE';
-import { Correlated } from 'interfaces/Correlation';
-
-const { projects } = Pyyne.Types;
-
-export default new (class ProjectService extends Pyyne.Service {
-  public async GetProjects(params?: GetProjectsParams): Promise<Project[]> {
-    const result = await Pyyne.Service.get<PYYNE.Project[]>('/projects', {
-      params: getProjects.params.out(params),
-    });
-
-    return result.data.map(projects.in);
-  }
-
-  public async GetProject(id: Id, params?: GetProjectParams): Promise<Project> {
-    const result = await Pyyne.Service.get<PYYNE.Project>(\`/project/\${id}\`, {
-      params: getProjects.params.out(params),
-    });
-
-    return projects.in(result.data);
-  }
-
-  public async GetPaginatedProjects(
-    params: PaginationParams & GetProjectsParams,
-  ): Promise<Paginated<Project[]>> {
-    const result = await Pyyne.Service.get<PYYNE.Paginated<PYYNE.Project[]>>(
-      \`/projects/page/\${params.pageIndex}\`,
-      {
-        params: getProjects.params.out(params),
-      },
-    );
-
-    return result.paginated(projects.in);
-  }
-
-  public async GetProjectsByConsultants(
-    params: GetProjectsParams & ConsultantRelationQuery,
-  ): Promise<Correlated<Project[]>> {
-    const result = await Pyyne.Service.get<PYYNE.Paginated<PYYNE.Project[]>>(
-      \`/projects\`,
-      {
-        params: getProjects.params.out(params),
-      },
-    );
-
-    return result.correlated(projects.in);
-  }
-
-  public async GetPaginatedProjectsByConsultants(
-    params: GetProjectsParams & ConsultantRelationQuery & PaginationParams,
-  ): Promise<Correlated<Project[]> & Paginated<Project[]>> {
-    const result = await Pyyne.Service.get<
-      PYYNE.Paginated<PYYNE.Project[]> & PYYNE.Correlated<PYYNE.Project[]>
-    >(\`/projects\`, {
-      params: getProjects.params.out(params),
-    });
-
-    return {
-      ...result.correlated(projects.in),
-      ...result.paginated(projects.in),
-    };
-  }
-
-  public async CreateProject(form: CreateProjectForm): Promise<Project> {
-    const { data } = await Pyyne.Service.post<
-      PYYNE.Project,
-      PYYNE.Requests.PostProject.Body
-    >('/projects', createProject.body.out(form));
-
-    return projects.in(data);
-  }
-
-  public async EditProject(
-    id: number,
-    form: EditProjectForm,
-  ): Promise<Project> {
-    const { data } = await Pyyne.Service.patch<
-      PYYNE.Project,
-      PYYNE.Requests.PatchProject.Body
-    >(\`/projects/\${id}\`, editProject.body.out(form));
-
-    return projects.in(data);
-  }
-
-  public async DeleteProject(id: number): Promise<boolean> {
-    const { status } = await Pyyne.Service.delete(\`/projects/\${id}\`);
-
-    return status === 204;
-  }
-})();
-`;
 
 export function Example({ Playground }: Props) {
   const [{ width, height }, setSize] = useState({ width: 0, height: 0 });
@@ -106,7 +16,7 @@ export function Example({ Playground }: Props) {
   const [showColourControls, setShowColourControls] = useState(false);
   const theme = useState(IdeSim.themes.PYYNE);
 
-  const [code, setCode] = useState(test);
+  const [code, setCode] = useState(data.code);
 
   const [terminalText, setTerminalText] = useState<TerminalLine[]>(
     `$ node server.is && stripe listen
---
+++ src/Playground/data.ts
@@ -0,0 +1,91 @@
+export const code = `import * as Pyyne from 'interfaces/PYYNE';
+import { Correlated } from 'interfaces/Correlation';
+
+const { projects } = Pyyne.Types;
+
+export default new (class ProjectService extends Pyyne.Service {
+  public async GetProjects(params?: GetProjectsParams): Promise<Project[]> {
+    const result = await Pyyne.Service.get<PYYNE.Project[]>('/projects', {
+      params: getProjects.params.out(params),
+    });
+
+    return result.data.map(projects.in);
+  }
+
+  public async GetProject(id: Id, params?: GetProjectParams): Promise<Project> {
+    const result = await Pyyne.Service.get<PYYNE.Project>(\`/project/\${id}\`, {
+      params: getProjects.params.out(params),
+    });
+
+    return projects.in(result.data);
+  }
+
+  public async GetPaginatedProjects(
+    params: PaginationParams & GetProjectsParams,
+  ): Promise<Paginated<Project[]>> {
+    const result = await Pyyne.Service.get<PYYNE.Paginated<PYYNE.Project[]>>(
+      \`/projects/page/\${params.pageIndex}\`,
+      {
+        params: getProjects.params.out(params),
+      },
+    );
+
+    return result.paginated(projects.in);
+  }
+
+  public async GetProjectsByConsultants(
+    params: GetProjectsParams & ConsultantRelationQuery,
+  ): Promise<Correlated<Project[]>> {
+    const result = await Pyyne.Service.get<PYYNE.Paginated<PYYNE.Project[]>>(
+      \`/projects\`,
+      {
+        params: getProjects.params.out(params),
+      },
+    );
+
+    return result.correlated(projects.in);
+  }
+
+  public async GetPaginatedProjectsByConsultants(
+    params: GetProjectsParams & ConsultantRelationQuery & PaginationParams,
+  ): Promise<Correlated<Project[]> & Paginated<Project[]>> {
+    const result = await Pyyne.Service.get<
+      PYYNE.Paginated<PYYNE.Project[]> & PYYNE.Correlated<PYYNE.Project[]>
+    >(\`/projects\`, {
+      params: getProjects.params.out(params),
+    });
+
+    return {
+      ...result.correlated(projects.in),
+      ...result.paginated(projects.in),
+    };
+  }
+
+  public async CreateProject(form: CreateProjectForm): Promise<Project> {
+    const { data } = await Pyyne.Service.post<
+      PYYNE.Project,
+      PYYNE.Requests.PostProject.Body
+    >('/projects', createProject.body.out(form));
+
+    return projects.in(data);
+  }
+
+  public async EditProject(
+    id: number,
+    form: EditProjectForm,
+  ): Promise<Project> {
+    const { data } = await Pyyne.Service.patch<
+      PYYNE.Project,
+      PYYNE.Requests.PatchProject.Body
+    >(\`/projects/\${id}\`, editProject.body.out(form));
+
+    return projects.in(data);
+  }
+
+  public async DeleteProject(id: number): Promise<boolean> {
+    const { status } = await Pyyne.Service.delete(\`/projects/\${id}\`);
+
+    return status === 204;
+  }
+})();
+`;
